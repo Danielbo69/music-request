716593b03c6159312db0dc02cb50c43a63b0fef9
Daniel Boscan <danielboscanramos@gmail.com>
2024-02-03 02:28:23 -0300

    sppinner listo y icono

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useState } from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Spinner from "./components/Spinner/Spinner";
 
 function App() {
-  return <Home />;
+  const [spinner, setSpinner] = useState(true);
+
+  setTimeout(function () {
+    setSpinner(false);
+  }, 5000);
+
+  if (!spinner) {
+    return <Home />;
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default App;
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Spinner.css";
 
 function Spinner() {
   return (
-    <div>Spinner</div>
-  )
+    <div className="w-100 d-flex justify-content-center align-items-center" style={{height: '100vh', backgroundColor: '#161616'}}>
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
-export default Spinner
+export default Spinner;
